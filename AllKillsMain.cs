f057810b159bb01b3e32bf1c48ecacd2e912a245
GGPC <GGPC@GGPC>
2024-08-25 12:44:40 +1200

    Add project files.

---
+++ AllKillsMain.cs
@@ -0,0 +1,160 @@
+﻿using BepInEx;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AllKills
+{
+    /// <summary>
+    ///     The main class for All Kills.
+    /// </summary>
+    [BepInPlugin(MOD_GUID, MOD_NAME, MOD_VERSION)]
+    public class AllKillsMain : BaseUnityPlugin
+    {
+        public const string MOD_GUID = "galva.all_kills";
+        public const string MOD_NAME = "Show All Kills";
+        public const string MOD_VERSION = "0.0.0";
+
+        private bool InStatisticsScreen = false;
+
+        private CreatureTemplate.Type _previousType = null;
+        private int _previousIntData = 0;
+
+        /// <summary>
+        ///     Run when mod is enabled.
+        /// </summary>
+        public void OnEnable()
+        {
+            On.CreatureSymbol.DoesCreatureEarnATrophy += EarnTrophyCheckHook;
+            On.Menu.StoryGameStatisticsScreen.KillsTable.KillSort += KillSortHook;
+
+            On.Menu.SleepScreenKills.ctor += SleepScreenConstructorHook;
+            On.Menu.StoryGameStatisticsScreen.KillsTable.ctor += KillsTableConstructorHook;
+
+            On.Menu.StoryGameStatisticsScreen.TickerIsDone += DoneHook;
+
+            On.RainWorld.LoadModResources += ResourceLoadHook;
+            On.RainWorld.UnloadResources += ResourceUnloadHook;
+            On.CreatureSymbol.SpriteNameOfCreature += SpriteNameHook;
+        }
+
+        private bool KillSortHook(
+            On.Menu.StoryGameStatisticsScreen.KillsTable.orig_KillSort orig,
+            Menu.StoryGameStatisticsScreen.KillsTable.KillTicker a,
+            Menu.StoryGameStatisticsScreen.KillsTable.KillTicker b)
+        {
+            return CreatureSort.GetCreatureValue(a.symbol.iconData) < CreatureSort.GetCreatureValue(b.symbol.iconData);
+        }
+
+        /// <summary>
+        ///     Hook for loading mod texture atlas.
+        /// </summary>
+        private void ResourceLoadHook(On.RainWorld.orig_LoadModResources orig, RainWorld self)
+        {
+            Futile.atlasManager.LoadAtlas("Atlases/iconsak");
+            orig(self);
+        }
+
+        /// <summary>
+        ///     Hook for unloading mod texture atlas.
+        /// </summary>
+        private void ResourceUnloadHook(On.RainWorld.orig_UnloadResources orig, RainWorld self)
+        {
+            Futile.atlasManager.UnloadAtlas("Atlases/iconsak");
+            orig(self);
+        }
+
+        /// <summary>
+        ///     Hook for finding custom creature kill icons.
+        /// </summary>
+        /// <returns> The icon. </returns>
+        private string SpriteNameHook(On.CreatureSymbol.orig_SpriteNameOfCreature orig, IconSymbol.IconSymbolData iconData)
+        {
+            if (iconData.critType == CreatureTemplate.Type.TempleGuard)
+                return "Kill_Guard";
+            if (iconData.critType == CreatureTemplate.Type.SmallCentipede)
+                return "Kill_SmallCentipede";
+
+            return orig(iconData);
+        }
+
+        /// <summary>
+        ///     Hook for activating logic on the sleep screen.
+        /// </summary>
+        private void SleepScreenConstructorHook(
+            On.Menu.SleepScreenKills.orig_ctor orig,
+            Menu.SleepScreenKills self,
+            Menu.Menu menu,
+            Menu.MenuObject owner,
+            Vector2 pos,
+            List<PlayerSessionRecord.KillRecord> killsData)
+        {
+            InStatisticsScreen = true;
+            orig(self, menu, owner, pos, killsData);
+            InStatisticsScreen = false;
+        }
+
+        /// <summary>
+        ///     Hook for activating logic on the final kill count table. </summary>
+        private void KillsTableConstructorHook(
+            On.Menu.StoryGameStatisticsScreen.KillsTable.orig_ctor orig,
+            Menu.StoryGameStatisticsScreen.KillsTable self,
+            Menu.Menu menu,
+            Menu.MenuObject owner,
+            Vector2 pos,
+            List<KeyValuePair<IconSymbol.IconSymbolData, int>> killsData)
+        {
+            InStatisticsScreen = true;
+            orig(self, menu, owner, pos, killsData);
+            InStatisticsScreen = false;
+
+            _previousType = null;
+            _previousIntData = 0;
+        }
+
+        /// <summary>
+        ///     Hook for returning scores of added creatures. Prevents a possible softlock on the results screen.
+        /// </summary>
+        private void DoneHook(On.Menu.StoryGameStatisticsScreen.orig_TickerIsDone orig, Menu.StoryGameStatisticsScreen self, Menu.StoryGameStatisticsScreen.Ticker ticker)
+        {
+            if(ticker.ID == Menu.StoryGameStatisticsScreen.TickerID.Kill)
+            {
+                IconSymbol.IconSymbolData iconData = (ticker as Menu.StoryGameStatisticsScreen.KillsTable.KillTicker).symbol.iconData;
+                if (iconData.critType == CreatureTemplate.Type.VultureGrub ||
+                    iconData.critType == CreatureTemplate.Type.Hazer ||
+                    iconData.critType == CreatureTemplate.Type.TubeWorm ||
+                    iconData.critType == CreatureTemplate.Type.SmallNeedleWorm ||
+                    iconData.critType == CreatureTemplate.Type.SmallCentipede ||
+                    iconData.critType == CreatureTemplate.Type.Deer ||
+                    iconData.critType == CreatureTemplate.Type.Overseer ||
+                    iconData.critType == CreatureTemplate.Type.TempleGuard ||
+                    iconData.critType == CreatureTemplate.Type.GarbageWorm ||
+                    iconData.critType == CreatureTemplate.Type.StandardGroundCreature ||
+                    iconData.critType == CreatureTemplate.Type.Fly ||
+                    iconData.critType == CreatureTemplate.Type.Leech ||
+                    iconData.critType == CreatureTemplate.Type.SeaLeech ||
+                    iconData.critType == CreatureTemplate.Type.Spider ||
+                    (ModManager.MSC && iconData.critType == MoreSlugcats.MoreSlugcatsEnums.CreatureTemplateType.JungleLeech) ||
+                    (iconData.critType == _previousType && iconData.intData == _previousIntData)) // Catch-all
+                {
+                    self.scoreKeeper.AddScoreAdder(0, ticker.getToValue);
+                    return;
+                }
+
+                _previousType = iconData.critType;
+                _previousIntData = iconData.intData;
+            }
+            orig(self, ticker);
+        }
+
+        /// <summary>
+        ///     Hook to remove filtering for creature kills.
+        /// </summary>
+        public bool EarnTrophyCheckHook(On.CreatureSymbol.orig_DoesCreatureEarnATrophy orig, CreatureTemplate.Type creature)
+        {
+            if (InStatisticsScreen)
+                return creature.Index != -1;
+            else
+                return orig(creature);
+        }
+    }
+}
---
+++ CreatureSort.cs
@@ -0,0 +1,82 @@
+﻿namespace AllKills
+{
+    /// <summary>
+    ///     Utility class for sorting creatures on the end screen.
+    /// </summary>
+    internal class CreatureSort
+    {
+        /// <summary>
+        ///     Get the ordering for the creatures.
+        /// </summary>
+        /// <param name="creature"> The creature type. </param>
+        /// <returns> Number representing the order. </returns>
+        public static int GetCreatureValue(IconSymbol.IconSymbolData creature)
+        {
+            CreatureTemplate.Type type = creature.critType;
+
+            if (type == CreatureTemplate.Type.Slugcat)                                      return 1;
+            if (type == MoreSlugcats.MoreSlugcatsEnums.CreatureTemplateType.SlugNPC)        return 2;
+            if (type == MoreSlugcats.MoreSlugcatsEnums.CreatureTemplateType.HunterDaddy)    return 3;
+            if (type == CreatureTemplate.Type.GreenLizard)                                  return 4;
+            if (type == CreatureTemplate.Type.PinkLizard)                                   return 5;
+            if (type == CreatureTemplate.Type.BlueLizard)                                   return 6;
+            if (type == CreatureTemplate.Type.WhiteLizard)                                  return 7;
+            if (type == CreatureTemplate.Type.BlackLizard)                                  return 8;
+            if (type == CreatureTemplate.Type.YellowLizard)                                 return 9;
+            if (type == MoreSlugcats.MoreSlugcatsEnums.CreatureTemplateType.SpitLizard)     return 10;
+            if (type == MoreSlugcats.MoreSlugcatsEnums.CreatureTemplateType.ZoopLizard)     return 11;
+            if (type == CreatureTemplate.Type.CyanLizard)                                   return 12;
+            if (type == CreatureTemplate.Type.RedLizard)                                    return 13;
+            if (type == MoreSlugcats.MoreSlugcatsEnums.CreatureTemplateType.TrainLizard)    return 14;
+            if (type == CreatureTemplate.Type.Salamander)                                   return 15;
+            if (type == MoreSlugcats.MoreSlugcatsEnums.CreatureTemplateType.EelLizard)      return 16;
+            if (type == CreatureTemplate.Type.Fly)                                          return 17;
+            if (type == CreatureTemplate.Type.CicadaA)                                      return 18;
+            if (type == CreatureTemplate.Type.CicadaB)                                      return 19;
+            if (type == CreatureTemplate.Type.Snail)                                        return 20;
+            if (type == CreatureTemplate.Type.Leech)                                        return 21;
+            if (type == CreatureTemplate.Type.SeaLeech)                                     return 22;
+            if (type == MoreSlugcats.MoreSlugcatsEnums.CreatureTemplateType.JungleLeech)    return 23;
+            if (type == CreatureTemplate.Type.PoleMimic)                                    return 24;
+            if (type == CreatureTemplate.Type.TentaclePlant)                                return 25;
+            if (type == CreatureTemplate.Type.Scavenger)                                    return 26;
+            if (type == MoreSlugcats.MoreSlugcatsEnums.CreatureTemplateType.ScavengerElite) return 27;
+            if (type == MoreSlugcats.MoreSlugcatsEnums.CreatureTemplateType.ScavengerKing)  return 28;
+            if (type == CreatureTemplate.Type.VultureGrub)                                  return 29;
+            if (type == CreatureTemplate.Type.Vulture)                                      return 30;
+            if (type == CreatureTemplate.Type.KingVulture)                                  return 31;
+            if (type == CreatureTemplate.Type.SmallCentipede)                               return 32;
+            if (type == CreatureTemplate.Type.Centipede)                                    return 33 + RWCustom.Custom.IntClamp(creature.intData, 1, 3) - 1;
+            if (type == CreatureTemplate.Type.RedCentipede)                                 return 36;
+            if (type == CreatureTemplate.Type.Centiwing)                                    return 37;
+            if (type == MoreSlugcats.MoreSlugcatsEnums.CreatureTemplateType.AquaCenti)      return 38;
+            if (type == CreatureTemplate.Type.TubeWorm)                                     return 39;
+            if (type == CreatureTemplate.Type.Hazer)                                        return 40;
+            if (type == CreatureTemplate.Type.LanternMouse)                                 return 41;
+            if (type == CreatureTemplate.Type.Spider)                                       return 42;
+            if (type == CreatureTemplate.Type.BigSpider)                                    return 43;
+            if (type == MoreSlugcats.MoreSlugcatsEnums.CreatureTemplateType.MotherSpider)   return 44;
+            if (type == CreatureTemplate.Type.SpitterSpider)                                return 45;
+            if (type == CreatureTemplate.Type.MirosBird)                                    return 46;
+            if (type == MoreSlugcats.MoreSlugcatsEnums.CreatureTemplateType.MirosVulture)   return 47;
+            if (type == CreatureTemplate.Type.BrotherLongLegs)                              return 48;
+            if (type == CreatureTemplate.Type.DaddyLongLegs)                                return 49;
+            if (type == MoreSlugcats.MoreSlugcatsEnums.CreatureTemplateType.TerrorLongLegs) return 50;
+            if (type == MoreSlugcats.MoreSlugcatsEnums.CreatureTemplateType.Inspector)      return 51;
+            if (type == CreatureTemplate.Type.Deer)                                         return 52;
+            if (type == CreatureTemplate.Type.EggBug)                                       return 53;
+            if (type == MoreSlugcats.MoreSlugcatsEnums.CreatureTemplateType.FireBug)        return 54;
+            if (type == CreatureTemplate.Type.DropBug)                                      return 55;
+            if (type == MoreSlugcats.MoreSlugcatsEnums.CreatureTemplateType.StowawayBug)    return 56;
+            if (type == CreatureTemplate.Type.SmallNeedleWorm)                              return 57;
+            if (type == CreatureTemplate.Type.BigNeedleWorm)                                return 58;
+            if (type == CreatureTemplate.Type.JetFish)                                      return 59;
+            if (type == MoreSlugcats.MoreSlugcatsEnums.CreatureTemplateType.Yeek)           return 60;
+            if (type == CreatureTemplate.Type.BigEel)                                       return 61;
+            if (type == MoreSlugcats.MoreSlugcatsEnums.CreatureTemplateType.BigJelly)       return 62;
+            if (type == CreatureTemplate.Type.TempleGuard)                                  return 63;
+
+            return 1000;
+        }
+    }
+}
