c0cd017bf367afd99d83aad5d799550721595047
Jaymon Furniss <jaymonfurniss123@gmail.com>
2025-05-11 19:06:48 +1200

    Re-done Implementation with IL Hooking

--- AllKillsMain.cs
+++ AllKillsMain.cs
@@ -1,7 +1,11 @@
 ﻿using BepInEx;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using Menu;
+using MonoMod.Cil;
 using UnityEngine;
+using System;
+using Mono.Cecil.Cil;
 
 namespace AllKills
 {
@@ -12,109 +16,118 @@ namespace AllKills
     [BepInPlugin(ModGuid, ModName, ModVersion)]
     public class AllKillsMain : BaseUnityPlugin
     {
+        #region Constants
+
         public const string ModGuid = "galva.all_kills";
         public const string ModName = "Show All Kills";
         public const string ModVersion = "1.0.3";
 
-        private bool _inStatisticsScreen;
+        #endregion
 
-        private CreatureTemplate.Type _previousType;
-        private int _previousIntData;
+        #region Private Properties
+
+        private readonly Statistics _statistics = new Statistics();
+
+        #endregion
 
         /// <summary>
-        ///     Run when mod is enabled.
+        ///     The entry point for the mod initialization. All method hooking is done here.
         /// </summary>
         public void OnEnable()
         {
+            // Earn trophy IL hooks
+            IL.Menu.StoryGameStatisticsScreen.KillsTable.ctor += ILHook_EarnTrophy;
+            IL.Menu.SleepScreenKills.ctor += ILHook_EarnTrophy;
+
             // Show all Kills
-            On.CreatureSymbol.DoesCreatureEarnATrophy += EarnTrophyCheckHook;
-            On.Menu.StoryGameStatisticsScreen.TickerIsDone += DoneHook;
-            On.Menu.StoryGameStatisticsScreen.KillsTable.KillSort += KillSortHook;
+            On.Menu.StoryGameStatisticsScreen.TickerIsDone += Hook_TickerIsDone;
+            On.Menu.StoryGameStatisticsScreen.KillsTable.KillSort += Hook_KillSort;
 
             // Resources
-            On.RainWorld.LoadModResources += ResourceLoadHook;
-            On.RainWorld.UnloadResources += ResourceUnloadHook;
-            On.CreatureSymbol.SpriteNameOfCreature += SpriteNameHook;
+            On.RainWorld.LoadModResources += Hook_LoadModResources;
+            On.RainWorld.UnloadResources += Hook_UnloadResources;
+            On.CreatureSymbol.SpriteNameOfCreature += Hook_SpriteNameOfCreature;
 
-            // Enable / Disable
-            On.Menu.SleepScreenKills.ctor += SleepScreenConstructorHook;
-            On.Menu.StoryGameStatisticsScreen.KillsTable.ctor += KillsTableConstructorHook;
+            // Statistics
+            _statistics.Attach();
         }
 
         #region Show All Kills
 
         /// <summary>
-        ///     Hook to remove filtering for creature kills.
+        ///     IL Hook: Declare all creatures as awarding a trophy. Will apply directly after <see cref="CreatureSymbol.DoesCreatureEarnATrophy"/>.
         /// </summary>
-        ///
-        /// <param name="orig"> Reference to original method. </param>
-        /// <param name="creature"> The creature template to check. </param>
-        ///
-        /// <returns>
-        ///     If the creature is a trophy kill. For this mod, all creatures with a valid index are accepted.
-        /// </returns>
-        public bool EarnTrophyCheckHook(On.CreatureSymbol.orig_DoesCreatureEarnATrophy orig,
-            CreatureTemplate.Type creature)
+        /// <param name="context">
+        ///     The intermediate language context.
+        /// </param>
+        private static void ILHook_EarnTrophy(ILContext context)
         {
-            if (_inStatisticsScreen)
-                return creature.Index != -1;
+            ILCursor cursor = new ILCursor(context);
 
-            return orig(creature);
+            cursor.GotoNext(
+                x => x.MatchCall<CreatureSymbol>("DoesCreatureEarnATrophy")
+            );
+            cursor.Index += 1;
+            cursor.Emit(OpCodes.Pop);
+            cursor.Emit(OpCodes.Ldc_I4_1);
         }
 
         /// <summary>
-        ///     Hook for returning scores of added creatures. Prevents a possible soft-lock on the results screen.
+        ///     Hook: This will return scores or 0 for creatures that do not have a kill score specified. This prevents the game from getting stuck
+        ///     trying to find the score for creatures that do not award any.
         /// </summary>
-        private void DoneHook(
+        /// <param name="orig">
+        ///     The original method being hooked to which is called once a creature counter on the statistics screen has finished counting up.
+        ///     This method is called to add the score to the score adder on the right side of the screen.
+        /// </param>
+        /// <param name="self">
+        ///     The statistics screen instance.
+        /// </param>
+        /// <param name="ticker">
+        ///     The ticker that just finished counting up. This will contain the creature data.
+        /// </param>
+        private static void Hook_TickerIsDone(
             On.Menu.StoryGameStatisticsScreen.orig_TickerIsDone orig,
-            Menu.StoryGameStatisticsScreen self,
-            Menu.StoryGameStatisticsScreen.Ticker ticker)
+            StoryGameStatisticsScreen self,
+            StoryGameStatisticsScreen.Ticker ticker)
         {
-            if (ticker.ID == Menu.StoryGameStatisticsScreen.TickerID.Kill)
+            if (ticker.ID == StoryGameStatisticsScreen.TickerID.Kill)
             {
                 IconSymbol.IconSymbolData? iconData =
-                    (ticker as Menu.StoryGameStatisticsScreen.KillsTable.KillTicker)?.symbol.iconData;
+                    (ticker as StoryGameStatisticsScreen.KillsTable.KillTicker)?.symbol.iconData;
 
                 if (iconData != null)
                 {
-                    if (iconData.Value.critType == CreatureTemplate.Type.VultureGrub ||
-                        iconData.Value.critType == CreatureTemplate.Type.Hazer ||
-                        iconData.Value.critType == CreatureTemplate.Type.TubeWorm ||
-                        iconData.Value.critType == CreatureTemplate.Type.SmallNeedleWorm ||
-                        iconData.Value.critType == CreatureTemplate.Type.SmallCentipede ||
-                        iconData.Value.critType == CreatureTemplate.Type.Deer ||
-                        iconData.Value.critType == CreatureTemplate.Type.Overseer ||
-                        iconData.Value.critType == CreatureTemplate.Type.TempleGuard ||
-                        iconData.Value.critType == CreatureTemplate.Type.GarbageWorm ||
-                        iconData.Value.critType == CreatureTemplate.Type.StandardGroundCreature ||
-                        iconData.Value.critType == CreatureTemplate.Type.Fly ||
-                        iconData.Value.critType == CreatureTemplate.Type.Leech ||
-                        iconData.Value.critType == CreatureTemplate.Type.SeaLeech ||
-                        iconData.Value.critType == CreatureTemplate.Type.Spider ||
-                        (ModManager.MSC && iconData.Value.critType ==
-                            DLCSharedEnums.CreatureTemplateType.JungleLeech) ||
-                        (iconData.Value.critType == _previousType &&
-                         iconData.Value.intData == _previousIntData)) // Catch-all
+                    if ((int)MultiplayerUnlocks.SandboxUnlockForSymbolData(iconData.Value) >= self.killScores.Length)
                     {
-                        self.scoreKeeper.AddScoreAdder(0,
-                            ticker.getToValue); // The game handles this appropriately by not adding a score adder
+                        self.scoreKeeper.AddScoreAdder(0, ticker.getToValue);
                         return;
                     }
-
-                    _previousType = iconData.Value.critType;
-                    _previousIntData = iconData.Value.intData;
                 }
             }
 
             orig(self, ticker);
         }
 
-        private static bool KillSortHook(
+        /// <summary>
+        ///     Hook: This will sort the kills in a more sensible order. The original kill sorter will sort the kills based on arena ID which doesn't
+        ///     group creatures in a way that entirely makes sense.
+        /// </summary>
+        /// <param name="orig">
+        ///     The original method being hooked to which will be replaced with the new sorting method from this mod.
+        /// </param>
+        /// <param name="tickerA"> The first ticker. </param>
+        /// <param name="tickerB"> The second ticker. </param>
+        /// <returns>
+        ///     <c>true</c> if <c>tickerA</c> is before <c>tickerB</c>, <c>false</c> otherwise.
+        /// </returns>
+        private static bool Hook_KillSort(
             On.Menu.StoryGameStatisticsScreen.KillsTable.orig_KillSort orig,
-            Menu.StoryGameStatisticsScreen.KillsTable.KillTicker a,
-            Menu.StoryGameStatisticsScreen.KillsTable.KillTicker b)
+            StoryGameStatisticsScreen.KillsTable.KillTicker tickerA,
+            StoryGameStatisticsScreen.KillsTable.KillTicker tickerB)
         {
-            return CreatureSort.GetCreatureValue(a.symbol.iconData) < CreatureSort.GetCreatureValue(b.symbol.iconData);
+            return CreatureSort.GetCreatureValue(tickerA.symbol.iconData) <
+                   CreatureSort.GetCreatureValue(tickerB.symbol.iconData);
         }
 
         #endregion
@@ -122,9 +135,15 @@ namespace AllKills
         #region Resources
 
         /// <summary>
-        ///     Hook for loading mod texture atlas.
+        ///     Hook: Load the texture atlas for the custom textures from this mod.
         /// </summary>
-        private static void ResourceLoadHook(On.RainWorld.orig_LoadModResources orig, RainWorld self)
+        /// <param name="orig">
+        ///     The original method for loading resources.
+        /// </param>
+        /// <param name="self">
+        ///     The rain world instance.
+        /// </param>
+        private static void Hook_LoadModResources(On.RainWorld.orig_LoadModResources orig, RainWorld self)
         {
             // ReSharper disable once StringLiteralTypo
             Futile.atlasManager.LoadAtlas("Atlases/iconsak");
@@ -132,9 +151,15 @@ namespace AllKills
         }
 
         /// <summary>
-        ///     Hook for unloading mod texture atlas.
+        ///     Hook: Unload the texture atlas for the custom textures from this mod.
         /// </summary>
-        private static void ResourceUnloadHook(On.RainWorld.orig_UnloadResources orig, RainWorld self)
+        /// <param name="orig">
+        ///     The original method for unloading resources.
+        /// </param>
+        /// <param name="self">
+        ///     The rain world instance.
+        /// </param>
+        private static void Hook_UnloadResources(On.RainWorld.orig_UnloadResources orig, RainWorld self)
         {
             // ReSharper disable once StringLiteralTypo
             Futile.atlasManager.UnloadAtlas("Atlases/iconsak");
@@ -142,16 +167,18 @@ namespace AllKills
         }
 
         /// <summary>
-        ///     Hook for finding custom creature kill icons.
+        ///     Hook: Lets the game find the sprites for the newly added creature textures.
         /// </summary>
-        /// 
-        /// <param name="orig"> Reference to original method. </param>
-        /// <param name="iconData"> The creature's icon data. </param>
-        /// 
+        /// <param name="orig">
+        ///     The original method that finds the sprite name for a creature.
+        /// </param>
+        /// <param name="iconData">
+        ///     The data of the creature whose sprite name should be returned.
+        /// </param>
         /// <returns>
-        ///     The custom icon if the input is a custom creature, otherwise the result of <c>orig(iconData)</c>.
+        ///     The sprite name of any newly added sprite when applicable, otherwise the original return value.
         /// </returns>
-        private static string SpriteNameHook(On.CreatureSymbol.orig_SpriteNameOfCreature orig,
+        private static string Hook_SpriteNameOfCreature(On.CreatureSymbol.orig_SpriteNameOfCreature orig,
             IconSymbol.IconSymbolData iconData)
         {
             if (iconData.critType == CreatureTemplate.Type.TempleGuard)
@@ -160,45 +187,6 @@ namespace AllKills
             return iconData.critType == CreatureTemplate.Type.SmallCentipede
                 ? "Kill_SmallCentipede"
                 : orig(iconData);
-        }
-
-        #endregion
-
-        #region Enable / Disable
-
-        /// <summary>
-        ///     Hook for enabling logic on the sleep screen.
-        /// </summary>
-        private void SleepScreenConstructorHook(
-            On.Menu.SleepScreenKills.orig_ctor orig,
-            Menu.SleepScreenKills self,
-            Menu.Menu menu,
-            Menu.MenuObject owner,
-            Vector2 pos,
-            List<PlayerSessionRecord.KillRecord> killsData)
-        {
-            _inStatisticsScreen = true;
-            orig(self, menu, owner, pos, killsData);
-            _inStatisticsScreen = false;
-        }
-
-        /// <summary>
-        ///     Hook enabling logic on the final kill count table.
-        /// </summary>
-        private void KillsTableConstructorHook(
-            On.Menu.StoryGameStatisticsScreen.KillsTable.orig_ctor orig,
-            Menu.StoryGameStatisticsScreen.KillsTable self,
-            Menu.Menu menu,
-            Menu.MenuObject owner,
-            Vector2 pos,
-            List<KeyValuePair<IconSymbol.IconSymbolData, int>> killsData)
-        {
-            _inStatisticsScreen = true;
-            orig(self, menu, owner, pos, killsData);
-            _inStatisticsScreen = false;
-
-            _previousType = null;
-            _previousIntData = 0;
         }
 
         #endregion
